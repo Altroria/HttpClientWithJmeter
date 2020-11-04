5cf6e0f240912dfd4e39dac9b52413c7acfab2fd
sunH <495648123@qq.com>
2020-11-04 09:27:41 +0800

    新增delete put请求

---
+++ src/main/java/com/tool/ClientWithResponseHandler.java
@@ -0,0 +1,169 @@
+package com.tool;
+
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+
+public class ClientWithResponseHandler {
+    /**
+     * 绕过验证
+     *
+     * @return
+     * @throws NoSuchAlgorithmException
+     * @throws KeyManagementException
+     */
+    public static SSLContext createIgnoreVerifySSL() throws NoSuchAlgorithmException, KeyManagementException {
+        SSLContext sc = SSLContext.getInstance("SSLv3");
+
+        // 实现一个X509TrustManager接口，用于绕过验证，不用修改里面的方法
+        X509TrustManager trustManager = new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(
+                    java.security.cert.X509Certificate[] paramArrayOfX509Certificate,
+                    String paramString) throws CertificateException {
+            }
+
+            @Override
+            public void checkServerTrusted(
+                    java.security.cert.X509Certificate[] paramArrayOfX509Certificate,
+                    String paramString) throws CertificateException {
+            }
+
+            @Override
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+        };
+
+        sc.init(null, new TrustManager[] { trustManager }, null);
+        return sc;
+    }
+
+    /**
+     * Get请求
+     * @param url
+     * @return
+     * @throws Exception
+     */
+    public  String doGet(String url) throws Exception {
+
+        String result = "";
+
+        //采用绕过验证的方式处理https请求
+        SSLContext sslcontext = createIgnoreVerifySSL();
+
+        //设置协议http和https对应的处理socket链接工厂的对象
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.INSTANCE)
+                .register("https", new SSLConnectionSocketFactory(sslcontext))
+                .build();
+        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+        HttpClients.custom().setConnectionManager(connManager);
+
+
+        //创建自定义的httpclient对象
+        CloseableHttpClient client = HttpClients.custom().setConnectionManager(connManager).build();
+        //CloseableHttpClient client = HttpClients.createDefault();
+
+        try{
+            //创建get方式请求对象
+            HttpGet get = new HttpGet(url);
+
+            //指定报文头Content-type、User-Agent
+            get.setHeader("Content-type", "application/json");
+
+            //执行请求操作，并拿到结果（同步阻塞）
+            CloseableHttpResponse response = client.execute(get);
+
+            //获取结果实体
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                //按指定编码转换结果实体为String类型
+                result = EntityUtils.toString(entity, "UTF-8");
+            }
+            EntityUtils.consume(entity);
+            //释放链接
+            response.close();
+            return result;
+        } catch (Exception e){
+            return "请求失败，再试一遍";
+        }
+        finally{
+            client.close();
+        }
+    }
+
+    /**
+     * Post请求
+     * @param url
+     * @param postBody
+     * @return
+     * @throws Exception
+     */
+    public String doPost(String url, String postBody) throws Exception {
+        String result = "";
+        //采用绕过验证的方式处理https请求
+        SSLContext sslcontext = createIgnoreVerifySSL();
+        //设置协议http和https对应的处理socket链接工厂的对象
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.INSTANCE)
+                .register("https", new SSLConnectionSocketFactory(sslcontext))
+                .build();
+        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+        HttpClients.custom().setConnectionManager(connManager);
+
+        //创建自定义的httpclient对象
+        CloseableHttpClient client = HttpClients.custom().setConnectionManager(connManager).build();
+        //CloseableHttpClient client = HttpClients.createDefault();
+        try{
+            //创建post方式请求对象
+            HttpPost httpPost = new HttpPost(url);
+            //指定报文头Content-type、User-Agent
+            httpPost.setHeader("Content-type", "application/json");
+            //设置body
+            httpPost.setEntity(new StringEntity(postBody, "utf-8"));
+
+            //执行请求操作，并拿到结果（同步阻塞）
+            CloseableHttpResponse response = client.execute(httpPost);
+            //获取结果实体
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                //按指定编码转换结果实体为String类型
+                result = EntityUtils.toString(entity, "UTF-8");
+            }
+            EntityUtils.consume(entity);
+            //释放链接
+            response.close();
+        }catch (Exception e){
+            e.printStackTrace();
+            result = "请求失败，再试一遍";
+        }
+        finally{
+            client.close();
+        }
+        return result;
+    }
+
+}
---
+++ src/main/java/com/tool/getUrl.java
@@ -0,0 +1,84 @@
+package com.tool;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.codec.binary.Hex;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class getUrl {
+    /**
+     *
+     * @param accessKey accessKey
+     * @param secretKey secretKey
+     * @param url url
+     * @param path path
+     * @param method method
+     * @return
+     * @throws UnsupportedEncodingException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     */
+   public String getRequestUrl(String accessKey,String secretKey,String url,String path,String method, String body) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException {
+       Map<String, String> parameters = new HashMap<>();
+       SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+       if (body != null){
+           //拆分字符串，获取body,put进parameters
+           String Body1 = body.substring(1, body.length() - 1);
+           String[] Body2 = Body1.split(",");
+           for (int i = 0; i <= Body2.length - 1; i++) {
+               String[] Body3 = Body2[i].split(":");
+//           System.out.println(Body3[0] + Body3[1]);
+               parameters.put(Body3[0].substring(1, Body3[0].length() - 1), Body3[1].substring(1, Body3[1].length() - 1));
+//           parameters.put(Body3[0], Body3[1]);
+           }
+       }
+
+
+       parameters.put("Version", "2016-12-05");
+       parameters.put("AccessKey", accessKey);
+       parameters.put("Timestamp", simpleDateFormat.format(new Date()));
+       parameters.put("SignatureMethod", "HmacSHA1");
+       parameters.put("SignatureVersion", "V2.0");
+       parameters.put("SignatureNonce", UUID.randomUUID().toString().replace("-", ""));
+
+       List<String> parameterKeyList = new ArrayList<>();
+       parameterKeyList.addAll(parameters.keySet());
+       Collections.sort(parameterKeyList);
+       List<String> list = new ArrayList<>();
+       for (String key : parameterKeyList) {
+           list.add(String.format("%s=%s", percentEncode(key), percentEncode(parameters.get(key))));
+       }
+       String canonicalizedQueryString = StringUtils.join(list, "&");
+       String stringToSign = method + "\n" + percentEncode(path) + "\n" + sha256Encode(canonicalizedQueryString);
+       String signature = hmacSha1Sign("BC_SIGNATURE&" + secretKey, stringToSign);
+       return url + path + "?" + canonicalizedQueryString + "&" + "Signature" + "=" + percentEncode(signature);
+   }
+
+   private String percentEncode(String data) throws UnsupportedEncodingException {
+       return data != null? URLEncoder.encode(data, "utf-8").replace("+", "%20").replace("*", "%2A").replace("%7E", "~") : null;
+   }
+
+   private String sha256Encode(String data) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+       MessageDigest digest = MessageDigest.getInstance("SHA-256");
+       byte[] hash = digest.digest(data.getBytes("utf-8"));
+       return Hex.encodeHexString(hash);
+   }
+
+   private String hmacSha1Sign(String secretKey,String data) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException {
+       Mac mac = Mac.getInstance("HmacSHA1");
+       byte[] secretKeyByte = secretKey.getBytes("utf-8");
+       SecretKeySpec secretKeySpec = new SecretKeySpec(secretKeyByte, "HmacSHA1");
+       mac.init(secretKeySpec);
+       return Hex.encodeHexString(mac.doFinal(data.getBytes("utf-8")));
+   }
+
+}
